17dcdd9a1ab3c14bfd2a4f1e6e3f95da894b8a29
aaa <xianglai2003@icloud.com>
2023-05-08 11:57:04 +0800

    试试

--- src/Card.java
+++ src/Card.java
@@ -1,23 +1,41 @@
 
 
 public class Card {
-    private String suit;
-    private String rank;
+    private String suit;//花色
+    private String rank;//大小
 
     public Card(String suit, String rank)
     {
         this.suit = suit;
         this.rank = rank;
     }
-
+    //setter and getter
     public String getsuit()
     {
         return suit;
+    }
+    public void setSuit(String suit) 
+    {
+        this.suit = suit;
     }
 
     public String getRank()
     {
         return rank;
+    }
+    public void setRank(String rank) 
+    {
+        this.rank = rank;
+    }
+    // 构造方法
+    public Card(){
+
+    }
+
+
+    public boolean equals(Card card)
+    {
+        return this.suit==sui&&this.rank==rank;
     }
 
     public String toString()
